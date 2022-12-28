e2e2244125a36e178194c70733b0677ee9632672
zumonhossain <zumon15-13169@diu.edu.bd>
2022-12-28 22:34:01 +0600

    Top Categories

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -17,3 +17,15 @@ $('.active-slider').owlCarousel({
         }
     }
 })
+
+
+// top-category-active
+
+$('.top-category-active').owlCarousel({
+	loop:true,
+	margin:24,
+	items:6,
+	navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+	nav:true,
+	dots:false,
+})
