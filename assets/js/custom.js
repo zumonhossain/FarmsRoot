8b5d82baf410c8229dee3aa03d0191f5ae0aad14
zumonhossain <zumon15-13169@diu.edu.bd>
2022-12-28 21:03:28 +0600

    header section & slider section

---
+++ assets/js/custom.js
@@ -0,0 +1,19 @@
+// owlCarousel
+$('.active-slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+		dots:true,
+		autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        767:{
+            items:1
+        },
+        992:{
+            items:1
+        }
+    }
+})
