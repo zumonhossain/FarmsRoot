878898e1ad467f95df06a2b3e81a7d8fa51247a9
zumonhossain <zumon15-13169@diu.edu.bd>
2022-12-30 12:07:59 +0600

    About Us

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -39,3 +39,421 @@ $('.new-product-active').owlCarousel({
 	dots:false,
 });
 
+
+
+
+
+
+
+
+const plus1 = document.querySelector(".plus1"),
+minus1 = document.querySelector(".minus1"),
+num1 = document.querySelector(".num1");
+
+let a1 = 1;
+
+plus1.addEventListener("click", () => {
+a1++;
+num1.innerText = a1;
+console.log(a1);
+});
+
+minus1.addEventListener("click", () => {
+if (a1 > 1) {
+  a1--;
+  num1.innerText = a1;
+}
+});
+
+
+
+
+
+const plus2 = document.querySelector(".plus2"),
+minus2 = document.querySelector(".minus2"),
+num2 = document.querySelector(".num2");
+
+let a2 = 1;
+
+plus2.addEventListener("click", () => {
+a2++;
+num2.innerText = a2;
+console.log(a2);
+});
+
+minus2.addEventListener("click", () => {
+if (a2 > 1) {
+  a2--;
+  num2.innerText = a2;
+}
+});
+
+
+
+
+const plus3 = document.querySelector(".plus3"),
+minus3 = document.querySelector(".minus3"),
+num3 = document.querySelector(".num3");
+
+let a3 = 1;
+
+plus3.addEventListener("click", () => {
+a3++;
+num3.innerText = a3;
+console.log(a3);
+});
+
+minus3.addEventListener("click", () => {
+if (a3 > 1) {
+  a3--;
+  num3.innerText = a3;
+}
+});
+
+
+
+const plus4 = document.querySelector(".plus4"),
+minus4 = document.querySelector(".minus4"),
+num4 = document.querySelector(".num4");
+
+let a4 = 1;
+
+plus4.addEventListener("click", () => {
+a4++;
+num4.innerText = a4;
+console.log(a4);
+});
+
+minus4.addEventListener("click", () => {
+if (a4 > 1) {
+  a4--;
+  num4.innerText = a4;
+}
+});
+
+
+
+const plus5 = document.querySelector(".plus5"),
+minus5 = document.querySelector(".minus5"),
+num5 = document.querySelector(".num5");
+
+let a5 = 1;
+
+plus5.addEventListener("click", () => {
+a5++;
+num5.innerText = a5;
+console.log(a5);
+});
+
+minus5.addEventListener("click", () => {
+if (a5 > 1) {
+  a5--;
+  num5.innerText = a5;
+}
+});
+
+
+
+const plus6 = document.querySelector(".plus6"),
+minus6 = document.querySelector(".minus6"),
+num6 = document.querySelector(".num6");
+
+let a6 = 1;
+
+plus6.addEventListener("click", () => {
+a6++;
+num6.innerText = a6;
+console.log(a6);
+});
+
+minus6.addEventListener("click", () => {
+if (a6 > 1) {
+  a6--;
+  num6.innerText = a6;
+}
+});
+
+
+
+const plus7 = document.querySelector(".plus7"),
+minus7 = document.querySelector(".minus7"),
+num7 = document.querySelector(".num7");
+
+let a7 = 1;
+
+plus7.addEventListener("click", () => {
+a7++;
+num7.innerText = a7;
+console.log(a7);
+});
+
+minus7.addEventListener("click", () => {
+if (a7 > 1) {
+  a7--;
+  num7.innerText = a7;
+}
+});
+
+
+
+const plus8 = document.querySelector(".plus8"),
+minus8 = document.querySelector(".minus8"),
+num8 = document.querySelector(".num8");
+
+let a8 = 1;
+
+plus8.addEventListener("click", () => {
+a8++;
+num8.innerText = a8;
+console.log(a8);
+});
+
+minus8.addEventListener("click", () => {
+if (a8 > 1) {
+  a8--;
+  num8.innerText = a8;
+}
+});
+
+
+
+const plus9 = document.querySelector(".plus9"),
+minus9 = document.querySelector(".minus9"),
+num9 = document.querySelector(".num9");
+
+let a9 = 1;
+
+plus9.addEventListener("click", () => {
+a9++;
+num9.innerText = a9;
+console.log(a9);
+});
+
+minus9.addEventListener("click", () => {
+if (a9 > 1) {
+  a9--;
+  num9.innerText = a9;
+}
+});
+
+
+const plus10 = document.querySelector(".plus10"),
+minus10 = document.querySelector(".minus10"),
+num10 = document.querySelector(".num10");
+
+let a10 = 1;
+
+plus10.addEventListener("click", () => {
+a10++;
+num10.innerText = a10;
+console.log(a10);
+});
+
+minus10.addEventListener("click", () => {
+if (a10 > 1) {
+  a10--;
+  num10.innerText = a10;
+}
+});
+
+
+const plus11 = document.querySelector(".plus11"),
+minus11 = document.querySelector(".minus11"),
+num11 = document.querySelector(".num11");
+
+let a11 = 1;
+
+plus11.addEventListener("click", () => {
+a11++;
+num11.innerText = a11;
+console.log(a11);
+});
+
+minus11.addEventListener("click", () => {
+if (a11 > 1) {
+  a11--;
+  num11.innerText = a11;
+}
+});
+
+
+const plus12 = document.querySelector(".plus12"),
+minus12 = document.querySelector(".minus12"),
+num12 = document.querySelector(".num12");
+
+let a12 = 1;
+
+plus12.addEventListener("click", () => {
+a12++;
+num12.innerText = a12;
+console.log(a12);
+});
+
+minus12.addEventListener("click", () => {
+if (a12 > 1) {
+  a12--;
+  num12.innerText = a12;
+}
+});
+
+
+const plus13 = document.querySelector(".plus13"),
+minus13 = document.querySelector(".minus13"),
+num13 = document.querySelector(".num13");
+
+let a13 = 1;
+
+plus13.addEventListener("click", () => {
+a13++;
+num13.innerText = a13;
+console.log(a13);
+});
+
+minus13.addEventListener("click", () => {
+if (a13 > 1) {
+  a13--;
+  num13.innerText = a13;
+}
+});
+
+
+const plus14 = document.querySelector(".plus14"),
+minus14 = document.querySelector(".minus14"),
+num14 = document.querySelector(".num14");
+
+let a14 = 1;
+
+plus14.addEventListener("click", () => {
+a14++;
+num14.innerText = a14;
+console.log(a14);
+});
+
+minus14.addEventListener("click", () => {
+if (a14 > 1) {
+  a14--;
+  num14.innerText = a14;
+}
+});
+
+
+const plus15 = document.querySelector(".plus15"),
+minus15 = document.querySelector(".minus15"),
+num15 = document.querySelector(".num15");
+
+let a15 = 1;
+
+plus15.addEventListener("click", () => {
+a15++;
+num15.innerText = a15;
+console.log(a15);
+});
+
+minus15.addEventListener("click", () => {
+if (a15 > 1) {
+  a15--;
+  num15.innerText = a15;
+}
+});
+
+
+const plus16 = document.querySelector(".plus16"),
+minus16 = document.querySelector(".minus16"),
+num16 = document.querySelector(".num16");
+
+let a16 = 1;
+
+plus16.addEventListener("click", () => {
+a16++;
+num16.innerText = a16;
+console.log(a16);
+});
+
+minus16.addEventListener("click", () => {
+if (a16 > 1) {
+  a16--;
+  num16.innerText = a16;
+}
+});
+
+
+const plus17 = document.querySelector(".plus17"),
+minus17 = document.querySelector(".minus17"),
+num17 = document.querySelector(".num17");
+
+let a17 = 1;
+
+plus17.addEventListener("click", () => {
+a17++;
+num17.innerText = a17;
+console.log(a17);
+});
+
+minus17.addEventListener("click", () => {
+if (a17 > 1) {
+  a17--;
+  num17.innerText = a17;
+}
+});
+
+
+
+const plus18 = document.querySelector(".plus18"),
+minus18 = document.querySelector(".minus18"),
+num18 = document.querySelector(".num18");
+
+let a18 = 1;
+
+plus18.addEventListener("click", () => {
+a18++;
+num18.innerText = a18;
+console.log(a18);
+});
+
+minus18.addEventListener("click", () => {
+if (a18 > 1) {
+  a18--;
+  num18.innerText = a18;
+}
+});
+
+
+const plus19 = document.querySelector(".plus19"),
+minus19 = document.querySelector(".minus19"),
+num19 = document.querySelector(".num19");
+
+let a19 = 1;
+
+plus19.addEventListener("click", () => {
+a19++;
+num19.innerText = a19;
+console.log(a19);
+});
+
+minus19.addEventListener("click", () => {
+if (a19 > 1) {
+  a19--;
+  num19.innerText = a19;
+}
+});
+
+
+const plus20 = document.querySelector(".plus20"),
+minus20 = document.querySelector(".minus20"),
+num20 = document.querySelector(".num20");
+
+let a20 = 1;
+
+plus20.addEventListener("click", () => {
+a20++;
+num20.innerText = a20;
+console.log(a20);
+});
+
+minus20.addEventListener("click", () => {
+if (a20 > 1) {
+  a20--;
+  num20.innerText = a20;
+}
+});
+
